13c3d55b8ef7d6ffe3be0f9bfa40377bf5170c0c
SpamLark <51059253+SpamLark@users.noreply.github.com>
2023-07-31 10:23:43 +0100

    Order by datetime so nights display in date order

--- roll-with-it-api/services/gameNights.js
+++ roll-with-it-api/services/gameNights.js
@@ -6,7 +6,7 @@ async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
     `SELECT game_night_id, game_night_location, game_night_datetime 
-    FROM game_nights LIMIT ${offset},${config.listPerPage}`
+    FROM game_nights ORDER BY game_night_datetime LIMIT ${offset},${config.listPerPage}`
   );
   const data = helper.emptyOrRows(rows);
   const meta = {page};
