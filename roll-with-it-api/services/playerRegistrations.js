592ddcab1bf048a00c7b690f2dd371425a50fe28
SpamLark <51059253+SpamLark@users.noreply.github.com>
2023-08-02 09:53:29 +0100

    Added capacity check to createPlayerRegistration

--- roll-with-it-api/services/playerRegistrations.js
+++ roll-with-it-api/services/playerRegistrations.js
@@ -1,19 +1,27 @@
 const db = require('./db');
+const helper = require('../helper');
 
 async function createPlayerRegistration(registrationDetails){
-    const result = await db.query(
-      `INSERT INTO player_registrations (user_id, game_instance_id)      
-      VALUES
-      (?, ?)`, [registrationDetails.uid, registrationDetails.game_instance_id]
-    );
-  
-    let message = 'Error whilst recording player registration';
-  
-    if (result.affectedRows) {
-      message = 'Player registration recorded';
-    }
-    return {message};
+
+  const hasCapacity = await helper.hasCapacityMiddleware(registrationDetails.game_instance_id);
+
+  if (!hasCapacity) {
+    return {status: 409, message: 'This game is currently full.'}
   }
+
+  const result = await db.query(
+    `INSERT INTO player_registrations (user_id, game_instance_id)      
+    VALUES
+    (?, ?)`, [registrationDetails.uid, registrationDetails.game_instance_id]
+  );
+
+  let message = 'Error whilst recording player registration';
+
+  if (result.affectedRows) {
+    message = 'Player registration recorded';
+  }
+  return {message};
+}
 
 module.exports = {
     createPlayerRegistration
