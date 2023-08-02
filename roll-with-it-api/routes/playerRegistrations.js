de1e222f29066f8012b8e5049d3d8cbd24631deb
SpamLark <51059253+SpamLark@users.noreply.github.com>
2023-08-02 09:57:21 +0100

    Refactored to better handle statuses from endpoint

--- roll-with-it-api/routes/playerRegistrations.js
+++ roll-with-it-api/routes/playerRegistrations.js
@@ -6,7 +6,14 @@ const playerRegistrations = require('../services/playerRegistrations');
 router.post('/', async function(req, res, next) {
     try {
       console.log(req.body);
-      res.json(await playerRegistrations.createPlayerRegistration(req.body));
+      const result = await playerRegistrations.createPlayerRegistration(req.body);
+      if (result.status === 200) {
+        res.status(200).json({message: result.message});
+      } else if (result.status === 409) {
+        res.status(409).json({message: result.message});
+      } else {
+        res.status(500).json({message: result.message});
+      }
     } catch (err) {
       console.error(`Error while creating player registration`, err.message);
       next(err);
